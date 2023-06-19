e71d7432ffd64838a097bc0c435bb086d7dfa20c
SolMon9099 <maximovic9099@gmail.com>
2023-06-19 12:14:44 -0400

    update home page of patient page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,18 +17,24 @@ class HomeController extends Controller
 
     public function index()
     {
-        $data = PatientTransaction::all();
-        $technicians = User::whereHas(
-            'roles', function($q){
-                $q->where('name', 'technician');
-            }
-        )->get()->all();
-        $doctors = User::whereHas(
-            'roles', function($q){
-                $q->where('name', 'doctor');
-            }
-        )->get()->all();
-        $schedules = PatientSchedule::query()->where('start_date', '>=', date('Y-m-d'))->get()->all();
-        return view('home', compact('data', 'technicians', 'doctors', 'schedules'));
+        if (auth()->user()->roles[0]->name == 'patient'){
+            return redirect(route('profiles.patient_transaction'));
+
+        } else {
+            $data = PatientTransaction::all();
+            $technicians = User::whereHas(
+                'roles', function($q){
+                    $q->where('name', 'technician');
+                }
+            )->get()->all();
+            $doctors = User::whereHas(
+                'roles', function($q){
+                    $q->where('name', 'doctor');
+                }
+            )->get()->all();
+            $schedules = PatientSchedule::query()->where('start_date', '>=', date('Y-m-d'))->get()->all();
+            return view('home', compact('data', 'technicians', 'doctors', 'schedules'));
+        }
+
     }
 }
